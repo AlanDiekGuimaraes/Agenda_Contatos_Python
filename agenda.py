e214688c998b8151abd1f65fd01947adc0e8de58
Alan Diek Guimarães <alandiekguimaraes@gmail.com>
2024-06-30 16:51:33 -0300

    Primeiros passos da agenda.

--- agenda.py
+++ agenda.py
@@ -1,1 +1,23 @@
-print('Alan')
+AGENDA = {}  # Criando uma Agenda vazia.
+AGENDA['Alan'] = {
+    'telefone': '1193454-3030',
+    'email': 'alan@alan.com',
+    'endereco': 'Rua: visconde'
+}
+AGENDA['Brenda'] = {
+    'telefone': '1193454-4040',
+    'email': 'brenda@brenda.com',
+    'endereco': 'Rua: Mende leal 154'
+}
+def mostrar_contatos(): # Criando um metodo para criar todo os contatos da agenda.
+    for contato in AGENDA:
+        buscar_contato(contato)
+        print(50*'-')
+
+def buscar_contato(contato):
+    print('Nome:', contato)
+    print('Telefone:', AGENDA[contato]['telefone'])
+    print('Email:', AGENDA[contato]['email'])
+    print('Endereço:', AGENDA[contato]['endereco'])
+
+mostrar_contatos()
